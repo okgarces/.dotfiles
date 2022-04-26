b4725d8313645ef33755c2f871f7c0c7c03cd632
Oscar Kiyoshige Garces Aparicio <dkodness@gmail.com>
2022-04-26 18:36:49 +1000

    Add first changes and function to migrate to lua

---
+++ vim/lua/compe_settings.lua
@@ -0,0 +1,34 @@
+-- Compe Configuration
+local set = vim.opt
+
+compe_settings = {
+  enabled = true,
+  autocomplete = true,
+  debug = false,
+  min_length = 1,
+  preselect = 'enable',
+  throttle_time = 80,
+  source_timeout = 200,
+  resolve_timeout = 800,
+  incomplete_delay = 400,
+  max_abbr_width = 100,
+  max_kind_width = 100,
+  max_menu_width = 100,
+  documentation = true,
+
+  source = {
+    path = true,
+    buffer = true,
+    calc = true,
+    nvim_lsp = true,
+    nvim_lua = true,
+    vsnip = true,
+    ultisnips = true,
+    luasnip = true,
+    emoji = true,
+  }
+} 
+
+
+vim.g['compe']=compe_settings;
+set.completeopt= "menuone,noselect"
